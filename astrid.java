a5e832f36466e63b56e69ae1aa446be5e3b8f5ba
teamchorus <44192131+teamchorus@users.noreply.github.com>
2018-10-23 14:48:13 +0530

    Update astrid.java

--- astrid.java
+++ astrid.java
@@ -1,10 +1,10 @@
-package au.com.astrid.example.main
+package au.com.coke.example.main
 
 public class Test {
 
 static void main(String[] args){
 
-	System.out.println("Hello World");
+	System.out.println("Hello World " + 2b2f72e625e98b122aa0a295d53a2c67 );
 	
 	}
 }
